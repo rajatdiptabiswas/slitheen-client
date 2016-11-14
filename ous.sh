b04e715d898f2bccec6e031dde476e3445ceb430
cecylia <cbocovic@uwaterloo.ca>
2016-11-14 16:20:47 -0500

    updated OUS bash script

--- ous.sh
+++ ous.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-count=0
-while true
-do
-	count=$((count+1))
-	echo "Calling phantomJS fr the $count th time"
-	./phantomjs --ssl-callbacks=slitheen --ssl-protocol=tlsv1.2 --ssl-ciphers=DHE-RSA-AES256-GCM-SHA384 loadpage.js > out-$count-put.out
-done
-
+./phantomjs --ssl-callbacks=slitheen --ssl-protocol=tlsv1.2 --ssl-ciphers=ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384 ous.js > ous.out
