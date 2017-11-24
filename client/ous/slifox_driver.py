8363961f8a8d8be78da72a7c344aaed6908c616e
aemhlori <anna.lorimer@edu.uwaterloo.ca>
2017-11-24 14:00:27 -0500

    Exception handling and various bug fixes

--- client/ous/slifox_driver.py
+++ client/ous/slifox_driver.py
@@ -4,14 +4,24 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.common.exceptions import TimeoutException
 import time
 import random
 import numpy
 import logging
+from urlparse import urlparse
 
 # Logging`
 LOGFILE = "slifox.log"
 
+# Overt sites list
+OVERT_SITES = ["https://www.python.org", "https://www.youtube.com/"]
+
+# Browser Actions
+ACTIONS = ["link", "new_addr", "history", "download", "nop"]
+ACTION_PROBABILITIES = [0.451, 0.33, 0.098, 0.012, 0.109]
+
+# Dwell time distribution params
 WEIBULL_SCALE   = 30
 WEIBULL_SHAPE   = 0.75
 
@@ -19,52 +29,102 @@ class SliFoxDriver(object):
 
         def start(self):
 		logging.info("Starting Slifox Driver ...")
-		logging.info("Getting FF webdriver ... ")
 		binary = FirefoxBinary('/home/iang/firefox/obj-x86_64-pc-linux-gnu/dist/bin/firefox')
                 self.driver = webdriver.Firefox(firefox_binary = binary, executable_path = "/home/iang/geckodriver")
+		self.driver.set_page_load_timeout(30)
+		logging.info("Slifox Driver started")
 
         def stop(self):
 		logging.info("Shutting down Slifox Driver ...")
                 self.driver.close()
 
-        def read(self):
-            #body = self.driver.find_elements_by_xpath(".//html")
 
-            #body_size = body[0].size['height']
-            read_time = self.random_read()
+	# Generate next action
+	def action(self):
+		# Choose an action using the probability of the action
+		action = numpy.random.choice(ACTIONS, p=ACTION_PROBABILITIES)
+		logging.info("Next action: " + action)
+		if action == "link":
+			self.navigate_within_site()
+			return 0
+		elif action == "new_addr":	
+			self.navigate_to_new_site()
+			return 0
+		elif action == "history":
+			self.navigate_to_history()
+			return 0
+		elif action == "download":
+			self.download()
+			return 0
+		else:
+			return -1
 
-            logging.debug("Reading for " + str(read_time))
-            time.sleep(read_time)
+	# Use Weibull Distribution to generate dwell time
+	def dwell(self):
+		dwell_time = WEIBULL_SCALE * numpy.random.weibull(WEIBULL_SHAPE)
+		logging.info("Dwelling for " + str(dwell_time))
+		time.sleep(dwell_time)
 
-        def navigate_within_site(self, url):
-	    print("Navigating ...")
+ 	# Navigate to a link on the same site
+ 	def navigate_within_site(self):
+ 		current_url = self.driver.current_url
+		logging.info("Navigating within site ... ")
+ 		links = self.driver.find_elements_by_xpath("//a[@href]")
+		while True:
+ 			try:
+				link_url  = numpy.random.choice(links).get_attribute("href")
+ 			except:			
+				# Element may have become stale or some other error occurred
+				logging.info("Something was wrong with that link ... trying another one")
+				break
 
-            while True:
-                links = sfd.driver.find_elements_by_xpath("//a[@href]")
-                num_links = len(links)
-                rand = random.randint(0, num_links - 1)
-                link_url  = links[rand].get_attribute("href")
-                if url in link_url:
-                    logging.debug("Navigating to " + link_url)
-                    sfd.driver.get(link_url)
-                    break;
+			domain = get_domain(link_url)
+ 
+ 			if domain in current_url:
+ 				logging.debug("Navigating to " + link_url)
+ 				try:
+					logging.debug("Attempting to navigate within site ...")
+ 					self.driver.get(link_url)
+ 					break
+ 				except TimeoutException as tex:
+ 					logging.info("TimeoutException while navigating within site")
+        
+	def navigate_to_new_site(self):
+		logging.info("Navigating to new site ... ")
+		 
+		while True:
+			new_site = random.choice(OVERT_SITES)
+			current_domain = get_domain(self.driver.current_url)
+			if new_site not in current_domain:
+				try:
+					logging.info("Attempting to navigate to " + new_site)            	
+					self.driver.get(new_site)
+					break
+				except TimeoutException as tex:
+					logging.info("TimeoutException while trying to navigate to a new site")
 
-        def random_read(self):
-            dwell_time = numpy.random.weibull(WEIBULL_SHAPE*WEIBULL_SCALE)    
-            return dwell_time
+
+	def navigate_to_history(self):
+		self.driver.back()
+
+	def download(self):
+		self.dwell()
+
+# Misc helper functions
+ 
+def get_domain(url):
+	parsed_url = urlparse(url)
+	domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_url)
+	return domain
 
 if __name__ == "__main__":
 	logging.basicConfig(filename=LOGFILE, level=logging.DEBUG)
         sfd = SliFoxDriver()
 
-        sites = ["https://www.reddit.com/", "https://www.python.org"]
-
-        url = random.choice(sites)
-
+        url = random.choice(OVERT_SITES)
         sfd.start()
+	logging.info("Getting initial URL")
         sfd.driver.get(url)
         while True:
-
-            sfd.navigate_within_site(url)
-            sfd.read()
-
+		sfd.dwell()
+		sfd.action()
